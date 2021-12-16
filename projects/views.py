9cede5f8100f386832cf0fffa37ca33c45d7947a
Bibek Gautam <beebayk0001@gmail.com>
2021-12-16 18:47:29 +0545

    Model Forms

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .forms import ProjectForm
 from .models import Project
 
 
@@ -16,3 +16,8 @@ def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
     return render(request, 'projects/single-project.html',{'project':projectObj, 'tags': tags})
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
